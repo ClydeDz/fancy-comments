83af5118199489b6d672d892c3c4621de60ffaa7
clydedz <clyd94@gmail.com>
2018-02-06 00:22:44 +1300

    Updated the website content.

--- tests/FancyComments.TestWeb/FancyComments.TestWeb/TestClass.cs
+++ tests/FancyComments.TestWeb/FancyComments.TestWeb/TestClass.cs
@@ -3,36 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for TestClass
-/// </summary>
 public class TestClass
 {
 
     public TestClass()
-    {
-        //
-        // TODO: Add constructor logic here
-        //
-
-        /*
-        Enter a suitable multi line comment here. 
-        */
-
-        /*
-        Copyright (C) Contosso Inc. - All Rights Reserved
-        Unauthorized copying of this file, via any medium is strictly prohibited
-        Proprietary and confidential
-        Written by John Smith <john.smith@contosso.com>, January 1, 2018
-        */
-
-        /*
-        Home page for Contosso.
-
-        Date         Author      Change description
-        ***************************************
-        01 January 2018  John Smith    Initial draft.
-        */
-
+    {   
     }
+
+
+
+
+
+
+    // CTRL+K CTRL+X to bring the snippets intellisense
+
+    /// <summary>
+    /// Prints a greeting on the console.
+    /// </summary>
+    public void HelloWorld()
+    {
+        // do something
+    }
+
+
+
+
+
+
+
 }
